5d2366b1c249ef91949bf656a6a872b09308be8e
Cristian Molinari <cris.molinari96@gmail.com>
2023-03-05 18:16:53 +0100

    modified exercise1

--- _1/FootballPlayer.java
+++ _1/FootballPlayer.java
@@ -1,6 +1,7 @@
-package pack._1.exerciseTutor._2;
+package pack._1.exerciseTutor._1;
 
 public class FootballPlayer {
+    // ATTRIBUTI
     private static int newId = 0 ;
     private int id;
     private String name ;
@@ -10,6 +11,7 @@ public class FootballPlayer {
     private int  numAssist ;
     private Team teamMembership;
 
+    // CONSTRUCT
     FootballPlayer(String name, String surName, int numeroPresenze, int numGol, int numAssist) {
         this.id = ++newId;
         this.name = name;
@@ -23,8 +25,7 @@ public class FootballPlayer {
             this.name = name;
             this.surName = surName;
         }
-
-        // Getters
+     // GETTERS
     public int getId() {return id;}
     public String getName() {return name;}
     public String getSurName() {return surName;}
@@ -33,12 +34,13 @@ public class FootballPlayer {
     public int getNumAssist() {return numAssist;}
     public Team getTeamMembership() {return teamMembership;}
 
-    // Setter
+    // SETTERS --> CI SERVIRANNO PER SETTARE I GOL, ASSIST E PRESENZE
     public void setNumeroPresenze(int numeroPresenze) {this.numeroPresenze = numeroPresenze;}
     public void setNumGol(int numGol) {this.numGol = numGol;}
     public void setNumAssist(int numAssist) {this.numAssist = numAssist;}
     public void setTeamMembership(Team teamMembership) {this.teamMembership = teamMembership;}
 
+    // METHODS
     public Double score(){
         Double scorePlayer =  numGol + (0.75 * numAssist) / numeroPresenze;
         return scorePlayer;
--- _1/Main.java
+++ _1/Main.java
@@ -1,32 +1,28 @@
-package pack._1.exerciseTutor._2;
+package pack._1.exerciseTutor._1;
 
 import java.util.*;
 
 public class Main {
 
 public static void main(String[] args) {
-
-    FootballPlayer maradona = new FootballPlayer("Diego Armando", "Maradona");
-    FootballPlayer messi = new FootballPlayer("Lionel", "Messi");
-    FootballPlayer ronaldo = new FootballPlayer("Cristian", "Ronaldo");
-    FootballPlayer oshimen = new FootballPlayer("Victor", "Oshimen");
-    FootballPlayer cannavaro = new FootballPlayer("Fabio", "Cannavaro");
+//
+    FootballPlayer player1 = new FootballPlayer("Diego Armando", "Maradona");
+    FootballPlayer player2 = new FootballPlayer("Lionel", "Messi");
+    FootballPlayer player3 = new FootballPlayer("Victor", "Oshimen");
+    FootballPlayer player4 = new FootballPlayer("Cristiano", "Ronaldo");
+    FootballPlayer player5 = new FootballPlayer("Fabio", "Cannavaro");
 
     // Team A
-    HashSet<FootballPlayer> threePlayers = new HashSet<>(Set.of(maradona, messi, oshimen));
-//    threePlayers.addAll(Set.of(maradona,messi,oshimen)); // più rapido il modo di sopra
+    HashSet<FootballPlayer> bestPlayers = new HashSet<>(Set.of(player1, player2, player3));
+//    bestPlayers.addAll(Set.of(maradona,messi,oshimen)); // più rapido il modo di sopra
 
-    Team teamA = new Team("Team A", threePlayers);
+    Team teamA = new Team("Team A", bestPlayers);
 
-    // Team b
+    // Team B
     Team teamB = new Team(0, "teamB");
-    teamB.addPlayer(ronaldo);
-    teamB.addPlayer(cannavaro);
+    teamB.addPlayer(player4);
+    teamB.addPlayer(player5);
 
-//    teamA.printTeam();
-//    teamB.printTeam();
-
-    // Ho prima ciclato il mio team per avere accesso ai singolo item, e poi quando ho il singolo item posso accedere ai metodi di quella classe e popolarli.
     for (FootballPlayer t : teamA.getListPlayersTeam()) {
         t.setNumAssist((int) (Math.random() * 11));
         t.setNumGol((int) (Math.random() * 11));
@@ -39,7 +35,8 @@ public static void main(String[] args) {
     }
 
     System.out.println("-------------------------------");
-// Creazione Team C con i giocatori del team A + team B
+
+// Creazione Team C con i giocatori del teamA + teamB
     HashSet fPlayerTeamC = new HashSet();
     fPlayerTeamC.addAll(teamA.getListPlayersTeam());
     fPlayerTeamC.addAll(teamB.getListPlayersTeam());
